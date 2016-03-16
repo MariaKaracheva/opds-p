583585a44a0a5edb1116d8ea7339a591ccdef39c
nmitropo <xiexed@gmail.com>
2016-03-16 12:19:20 +0200

    getting query string

--- src/hello_world/core.clj
+++ src/hello_world/core.clj
@@ -1,6 +1,13 @@
-(ns hello-world.core)
+(ns hello-world.core
+  (:require [ring.util.response :refer [response content-type]]))
+
+;(require ['ring.util.response (:refer [response content-type ])])
 
 (defn handler [request]
-  {:status 200
+  {:status  200
    :headers {"Content-Type" "text/html"}
-   :body "Hello Worldff3"})
+   :body    (concat "Hello Worldff3" (:query-string request))})
+
+;(defn handler [request]
+;  (-> (response "Hello World6")
+;      (content-type "text/plain")))
