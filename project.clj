b62e62406e45552a785db76a0f63575b629601ae
xiexed <xiexed@gmail.com>
2020-02-02 12:14:06 +0300

    deps update

--- project.clj
+++ project.clj
@@ -2,23 +2,23 @@
 
 (defproject opdsp "1.0.0-SNAPSHOT"
   :description "FIXME: write"
-  :dependencies [[org.clojure/clojure "1.8.0"]
-                 [ring/ring-core "1.5.0"]
-                 [ring/ring-defaults "0.2.0"]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [ring/ring-core "1.8.0"]
+                 [ring/ring-defaults "0.3.2"]
                  [org.apache.httpcomponents/httpclient "4.5.2"]
-                 [lock-key "1.4.1"]
-                 [com.github.kyleburton/clj-xpath "1.4.5"]
+                 [lock-key "1.5.0"]
+                 [com.github.kyleburton/clj-xpath "1.4.11"]
                  [org.clojure/data.xml "0.0.8"]
-                 [compojure "1.5.0"]
-                 [clj-yaml "0.4.0"]
-                 [clj-http "3.0.1"]
-                 [com.cemerick/friend "0.2.1"]
-                 [com.novemberain/monger "3.1.0"]
+                 [compojure "1.6.1"]
+                 [clj-commons/clj-yaml "0.7.0"]
+                 [clj-http "3.10.0"]
+                 [com.cemerick/friend "0.2.3"]
+                 [com.novemberain/monger "3.5.0"]
                  [hiccup "1.0.5"]
-                 [org.clojure/data.json "0.2.6"]
+                 [org.clojure/data.json "0.2.7"]
                  ]
 
-  :plugins [[lein-ring "0.9.7"] [lein-auto "0.1.2"]]
+  :plugins [[lein-ring "0.12.5"] [lein-auto "0.1.3"]]
   :ring {:handler opdsp.core/standalone-routes
          :auto-reload? true
          :auto-refresh? true
@@ -27,7 +27,7 @@
              :uberjar {:ring {:handler opdsp.core/opds-p-handler}}
              :test {
                     :dependencies [
-                                   [ring/ring-jetty-adapter "1.4.0"]
+                                   [ring/ring-jetty-adapter "1.8.0"]
                                    ]
                     }
 
